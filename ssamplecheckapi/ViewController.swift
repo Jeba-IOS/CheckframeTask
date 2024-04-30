91ef6fb8cc0b38c9b1e5baf60eed6c09800ded73
Bareeth <B>
2024-04-30 20:53:00 +0530

    initial check - JEBA

--- ssamplecheckapi/ViewController.swift
+++ ssamplecheckapi/ViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
-
+import apicallpackage
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ConnectionHandler.shared.getRequest(for: <#T##String#>, APIUrl: <#T##String#>, params: <#T##Parameters#>).responseDecode(to: <#T##T#>, <#T##Closure<T>#>)
         // Do any additional setup after loading the view.
     }
 
